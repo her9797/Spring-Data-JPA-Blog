5cf8464e4bb344fe2182dcf2e01926eab7131078
her97 <simkoon0120@gmail.com>
2024-04-05 11:40:25 +0900

    html / css 작업

---
+++ src/main/java/com/practice/blog/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.practice.blog.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"main", "/"})
+    public String main(){
+        return "main";
+    }
+
+}
